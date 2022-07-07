a67e6183cc6cb40bd24f3d38f75d6accc81d79f8
TheoTollyTheobald <Theo.Theobald@nottscc.gov.uk>
2022-07-07 12:11:47 +0100

    changes

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -9,7 +9,7 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook (string name) : base(name)
+        public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
         }
@@ -18,34 +18,29 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5)
             {
-                throw new InvalidOperationException("Too few students for ranked grading!");
+                throw new InvalidOperationException("You must have at least 5 students to do ranked grading.");
             }
-            double percentileRank = (averageGrade / 100) * (Students.Count + 1);
 
-            // Works out how many students are in 20% - rounding up
             var threshold = (int)Math.Ceiling(Students.Count * 0.2);
-            // Orders all students in a list of descending grade and then takes a list of just those values
-            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList(); 
-            // Determine if argument is greater than or equal to the grade on the border of the 20th percentile 
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
-            if (averageGrade >= grades[threshold] - 1) // -1 because list starts at 0th element
+            if (averageGrade >= grades[threshold - 1])
             {
                 return 'A';
             }
-            if (averageGrade >= grades[(threshold * 2)] - 1)
+            else if (averageGrade >= grades[(threshold * 2) - 1])
             {
                 return 'B';
             }
-            if (averageGrade >= grades[(threshold * 3)] - 1)
+            else if (averageGrade >= grades[(threshold * 3) - 1])
             {
                 return 'C';
             }
-            if (averageGrade >= grades[(threshold * 4)] - 1)
+            else if (averageGrade >= grades[(threshold * 4) - 1])
             {
                 return 'D';
             }
-
-            return 'F';
+            else return 'F';
         }
     }
 }
