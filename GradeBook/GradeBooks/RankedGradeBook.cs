705fb5c4f3eaff02c81a574f9222043311945e22
TheoTollyTheobald <Theo.Theobald@nottscc.gov.uk>
2022-07-07 10:56:18 +0100

    this won't be right

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,5 +12,32 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Too few students for ranked grading!");
+            }
+            double percentileRank = (averageGrade / 100) * (Students.Count + 1);
+
+            if (percentileRank >= 80)
+            {
+                return 'A';
+            }
+            else if (percentileRank >= 60)
+            {
+                return 'B';
+            }
+            else if (percentileRank >= 40)
+            {
+                return 'C';
+            }
+            else if (percentileRank >= 20)
+            {
+                return 'D';
+            }
+            return 'F';
+        }
     }
 }
