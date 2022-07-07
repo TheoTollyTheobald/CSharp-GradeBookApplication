f258257d4091d0aa3fd0a9f080263f1a86f741e1
TheoTollyTheobald <Theo.Theobald@nottscc.gov.uk>
2022-07-07 11:31:19 +0100

    Needlessly finnicky about code structure

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -32,15 +32,15 @@ namespace GradeBook.GradeBooks
             {
                 return 'A';
             }
-            if (averageGrade >= grades[threshold*2] - 1)
+            if (averageGrade >= grades[(threshold * 2)] - 1)
             {
                 return 'B';
             }
-            if (averageGrade >= grades[threshold*3] - 1)
+            if (averageGrade >= grades[(threshold * 3)] - 1)
             {
                 return 'C';
             }
-            if (averageGrade >= grades[threshold*4] - 1)
+            if (averageGrade >= grades[(threshold * 4)] - 1)
             {
                 return 'D';
             }
